14fc940c17aa8916e8400627746574dd8e5d15c3
Juan Pablo Formento <juan_pablo94@outlook.com>
2022-06-27 13:09:52 -0300

    Create clase_de_prueba.java

---
+++ Conecta4/src/conecta4/clase_de_prueba.java
@@ -0,0 +1,16 @@
+package conecta4;
+
+public class clase_de_prueba {
+	
+	public int suma(){
+		int n = 2;
+		int m = 3;
+		int l = n + m;
+		if (l > 2){
+			return n;
+		}
+		else {
+			return n;}
+		}
+	
+}
